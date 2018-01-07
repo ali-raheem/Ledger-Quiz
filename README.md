adbb845a4ccdde29a56959e778ac3e3ed25d36f0
Ali Raheem <ali.raheem@gmail.com>
2018-01-07 02:21:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ledger Quiz
+
+No one can be bothered to read the Ledger website or FAQ before ordering and putting their money on it. So I thought. Hey. People like Quizzes?
+
+
+Anyways contributions welcome :)
---
+++ quiz.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#check1").click(function () {
+        $("#answer1").slideDown();
+    });   
+});
+
+$(document).ready(function() {
+    $("#check2").click(function () {
+        $("#answer2").slideDown();
+    });   
+});
+$(document).ready(function() {
+    $("#check3").click(function () {
+        $("#answer3").slideDown();
+    });   
+});
+$(document).ready(function() {
+    $("#check4").click(function () {
+        $("#answer4").slideDown();
+    });   
+});
+$(document).ready(function() {
+    $("#check5").click(function () {
+        $("#answer5").slideDown();
+    });   
+});
+
